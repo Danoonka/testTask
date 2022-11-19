4e14395dcbd0f4d267fda217666f6a6ae6f79f72
Danoonka <bogdanaostrovska986@gmail.com>
2022-11-19 16:51:52 +0200

    modifaying

--- src/components/jobTile/JobTile.jsx
+++ src/components/jobTile/JobTile.jsx
@@ -11,12 +11,12 @@ import {arrOfData} from "../../utils/zapaska";
 import Pagination from "./pagination/Pagination";
 
 
-const apiData = fetchData("https://api.json-generator.com/templates/ZM1r0eic3XEy/data?access_token=wm3gg940gy0xek1ld98uaizhz83c6rh2sir9f9fu");
+//const apiData = fetchData("https://api.json-generator.com/templates/ZM1r0eic3XEy/data?access_token=wm3gg940gy0xek1ld98uaizhz83c6rh2sir9f9fu");
 
 
 const JobTile = () => {
-    const data =  apiData.read();
-    //const data =  arrOfData;
+    //const data =  apiData.read();
+    const data =  arrOfData;
     const ref =  useRef(null);
 
     const [currentPage, setCurrentPage] = useState(1)
